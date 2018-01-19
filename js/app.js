f5dd92f4560f09c64d64d8c9674deb9abb0d3253
Natasha Bibbs <natashabibbs10@gmail.com>
2018-01-18 14:55:27 -1000

    pokedex update

--- js/app.js
+++ js/app.js
@@ -5,12 +5,15 @@
 // Find the div with the id of "name1" and replace the text n/a with "Tentacool"
 // Place your code below
 
+document.getElementById("name1").innerHTML = "Tentacool";
+
 
 
 // Pokemon 2
 // Find the div with the id of "type2" and replace the text n/a with "water"
 // Place your code below
 
+document.getElementById("type2").innerHTML = "water";
 
 
 
@@ -20,13 +23,14 @@
 
 
 
+document.getElementById("pokeNumber3").innerHTML = "116";
 
 
 // Pokemon 4
 // Find the div with the id of "bio4" and replace the text n/a with your personal description of this Pokemon.
 // Place your code below
 
-
+document.getElementById("bio4").innerHTML = "Ocean man, take me by the hand, lead me to the land that you understand";
 
 
 
@@ -35,7 +39,7 @@
 // Place your code below
 
 
-
+document.getElementById("bio5").style.color = "red";
 
 
 // Pokemon 6
@@ -43,6 +47,7 @@
 // Place your code below
 
 
+document.getElementById("name6").style.fontSize = "60px";
 
 
 
